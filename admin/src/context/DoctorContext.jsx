54e364eff654c072c6ebf9f7ba7e1043f4f3f130
Sudesh Kumar <infodoc419@gmail.com>
2025-09-06 03:01:12 +0530

    Update DoctorContext.jsx

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -8,7 +8,7 @@ export const DoctorContext = createContext()
 
 const DoctorContextProvider = (props)=>{
       
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = 'https://prescripto-backend-8ryp.onrender.com'
     const [dToken, setDToken] = useState(localStorage.getItem('dToken')?localStorage.getItem('dToken'):'')
     const [appointments,setAppointments] = useState([])
     const [dashData,setDashData] = useState(false)

74f1cc7e67f308b0ec7a42f04ce932d3e1c0ce94
Sudesh Kumar <infodoc419@gmail.com>
2025-09-28 21:21:50 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const currencySymbol = "$";
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = 'https://prescripto-backend-blue.vercel.app';
   const [doctors, setDoctors] = useState([]);
   const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):false);
   const [userData,setUserData] = useState(false)
